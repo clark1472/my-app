7527e8c00149f46fc8d6d1946d1b0c386d2d5b3f
Salsa <>
2019-06-26 14:11:23 -0700

    trying to get 12 photos up Different sizes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
-import React from 'react';
-import Navbar from "./components/Navbar";
-import Jumbotron from "./components/Jumbotron";
-import Card from "./components/Card";
+import React, { Component } from "react";
 // import logo from './logo.svg';
-import './App.css';
-// import HelloBootstrap from './components/HelloBootstrap';
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import friends from "./friends.json";
+// import './App.css';
 
-function App() {
-  return (
-  <div className="container">
-  <Navbar />
-  {/* <HelloBootstrap/> */}
-  <Jumbotron />
-  <Card />
-</div>
-);
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    friends
+  };
+
+  // I do not think I need this.
+  removeFriend = id => {
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    const friends = this.state.friends.filter(friend => friend.id !== id);
+    // Set this.state.friends equal to the new friends array
+    this.setState({ friends });
+  };
+ 
+
+   // Map over this.state.friends and render a FriendCard component for each friend object
+   render() {
+    return (
+      <Wrapper>
+        <Title>Friends List</Title>
+        {this.state.friends.map(friend => (
+          <FriendCard
+            removeFriend={this.removeFriend}
+            id={friend.id}
+            key={friend.id}
+            // name={friend.name}
+            image={friend.image}
+            occupation={friend.occupation}
+            location={friend.location}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+
 }
-
 export default App;
